74c83f681d38febb7c0263c44b304659f18cb66c
Karen Snyder <karen.d.snyder@advantagesolutions.net>
2019-09-05 14:50:57 -0700

    Display map centered on US and add marker for San Diego

--- js/index.ctrl.js
+++ js/index.ctrl.js
@@ -1,9 +1,45 @@
 $(document).ready(function () {
 
     mapboxgl.accessToken = 'pk.eyJ1Ijoia2RzbnlkZXJkZXYiLCJhIjoiY2swNnZ4ODB6MDEyZjNscGFuYXhnbzVrYyJ9.Yy8MxX3sxuGu0NVwxeFEJA';
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11'
-    });
 
+    var mapboxClient = mapboxSdk({ accessToken: mapboxgl.accessToken });
+    mapboxClient.geocoding.forwardGeocode({
+        query: 'Salina, Kansas',
+        autocomplete: false,
+        limit: 1
+    })
+        .send()
+        .then(function (response) {
+            if (response && response.body && response.body.features && response.body.features.length) {
+                console.log('geocode response: ');
+                console.log(response);
+                var centerFeature = response.body.features[0];
+
+                var map = new mapboxgl.Map({
+                    container: 'map',
+                    boxZoom: true,
+                    style: 'mapbox://styles/mapbox/streets-v11',
+                    center: centerFeature.center,
+                    zoom: 4
+                });
+
+                mapboxClient.geocoding.forwardGeocode({
+                    query: 'San Diego, California',
+                    autocomplete: false,
+                    limit: 1
+                })
+                    .send()
+                    .then(function (response) {
+                        if (response && response.body && response.body.features && response.body.features.length) {
+                            console.log('geocode response: ');
+                            console.log(response);
+                            var sdFeature = response.body.features[0];
+
+                            new mapboxgl.Marker()
+                                .setLngLat(sdFeature.center)
+                                .addTo(map);
+                        }
+                    });
+            }
+        });
 });
