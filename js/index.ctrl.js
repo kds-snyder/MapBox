6c3a4c3837bcd59293aac29953b502420e1054e9
Karen Snyder <karen.d.snyder@advantagesolutions.net>
2019-09-05 14:09:30 -0700

    First commit of MapBox from https://docs.mapbox.com/mapbox-gl-js/overview/

---
+++ js/index.ctrl.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia2RzbnlkZXJkZXYiLCJhIjoiY2swNnZ4ODB6MDEyZjNscGFuYXhnbzVrYyJ9.Yy8MxX3sxuGu0NVwxeFEJA';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11'
+    });
+
+});
